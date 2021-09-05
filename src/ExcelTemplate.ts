1cb6335e3244bcd5c9aaef6e319e5f6cf950e8e7
Yoshihiro Seki <nikochan2k@gmail.com>
2021-09-05 18:35:22 +0900

    generate excel file

--- src/ExcelTemplate.ts
+++ src/ExcelTemplate.ts
@@ -3,16 +3,17 @@ import * as fs from "fs";
 import { template } from "lodash";
 import { BinarySource, Converter, dataUrlToBase64 } from "univ-conv";
 
-const EXPR_REGEXP = /\${[^}]+}/;
+const EXPR_REGEXP = /<%[^%]+%>/;
+const URL_REGEXP = /^(https?|blob|data|file):/;
+const IMAGE_EXTENSIONS = /^(jpg|jpeg|png|gif)$/;
 
-interface CellPos {
-  col: number;
+interface CellIndex {
   row: number;
+  col: number;
 }
-
 interface Target {
-  tl: CellPos;
-  br: CellPos;
+  tl: CellIndex;
+  br: CellIndex;
   text?: string;
 }
 
@@ -38,7 +39,7 @@ export class ExcelTemplate {
     this.converter = new Converter();
   }
 
-  public async parse(data: any) {
+  public async generate(data: any): Promise<ArrayBuffer> {
     let buffer: ArrayBuffer | undefined;
     if (typeof this.xlsx === "string") {
       const urlLike = this.xlsx;
@@ -74,10 +75,7 @@ export class ExcelTemplate {
             if (!cell.isMerged && !EXPR_REGEXP.test(text)) {
               continue;
             }
-            target = {
-              tl: { row, col },
-              br: { row, col },
-            };
+            target = { tl: { row, col }, br: { row, col } };
             targetMap[key] = target;
           }
           if (!target.text) {
@@ -90,21 +88,55 @@ export class ExcelTemplate {
           }
         }
       }
-      console.log(targetMap);
+
+      for (const [address, target] of Object.entries(targetMap)) {
+        const text = target.text || "";
+        try {
+          if (URL_REGEXP.test(text)) {
+            const url = new URL(text);
+            if (url.hash === "#embed") {
+              const lastIndex = url.pathname.lastIndexOf(".");
+              let extension = url.pathname.substr(lastIndex + 1);
+              if (extension === "jpg") extension = "jpeg";
+              if (IMAGE_EXTENSIONS.test(extension)) {
+                const buffer = await this.fetchURL(url);
+                const imageId = workbook.addImage({
+                  buffer,
+                  extension: extension as any,
+                });
+
+                ws.addImage(imageId, {
+                  tl: { row: target.tl.row - 1, col: target.tl.col - 1 } as any,
+                  br: target.br as any,
+                });
+              }
+            }
+          }
+        } catch (e) {
+          console.warn(e);
+        }
+
+        const cell = ws.getCell(address);
+        const value: any = cell.value;
+        if (value.font) {
+          value.text = text;
+        } else {
+          cell.value = text;
+        }
+      }
     }
+
+    return workbook.xlsx.writeBuffer();
   }
 
   private async fetchURL(url: URL): Promise<ArrayBuffer | Buffer> {
-    if (
-      url.protocol === "http:" ||
-      url.protocol === "https:" ||
-      url.protocol === "blob:"
-    ) {
+    const proto = url.protocol;
+    if (proto === "http:" || proto === "https:" || proto === "blob:") {
       const fetched = await fetch(url.href);
       return fetched.arrayBuffer();
-    } else if (url.protocol === "file:") {
+    } else if (proto === "file:") {
       return this.readFile(url.pathname);
-    } else if (url.protocol === "data:") {
+    } else if (proto === "data:") {
       const base64 = dataUrlToBase64(url.href);
       return this.converter.toArrayBuffer({
         encoding: "Base64",
--- src/__tests__/test.spec.ts
+++ src/__tests__/test.spec.ts
@@ -1,15 +1,55 @@
 import { ExcelTemplate } from "../ExcelTemplate";
+import { writeFileSync } from "fs";
 import { pathToFileURL } from "url";
 import { join } from "path";
 
-test("readFile", async () => {
+test("test1", async () => {
   const path = join(__dirname, "test1.xlsx");
   const url = pathToFileURL(path);
   const template = new ExcelTemplate(url.href);
-  await template.parse({
+  const ab = await template.generate({
     data1: "fuga",
     data2: "hoge",
     data3: "foo",
     data4: "bar",
   });
+  const buffer = Buffer.from(ab);
+  const outpath = "tmp/test1_out.xlsx";
+  writeFileSync(outpath, buffer);
+});
+
+test("test2", async () => {
+  const path = join(__dirname, "test1.xlsx");
+  const url = pathToFileURL(path);
+  const template = new ExcelTemplate(url.href);
+  const imgPath = join(__dirname, "test.jpg");
+  const imgUrl = pathToFileURL(imgPath);
+  imgUrl.hash = "#embed";
+  console.log(imgUrl.href);
+  const ab = await template.generate({
+    data1: "fuga",
+    data2: "hoge",
+    data3: "foo",
+    data4: imgUrl.href,
+  });
+  const buffer = Buffer.from(ab);
+  const outpath = "tmp/test2_out.xlsx";
+  writeFileSync(outpath, buffer);
+});
+
+test("test3", async () => {
+  const path = join(__dirname, "test1.xlsx");
+  const url = pathToFileURL(path);
+  const template = new ExcelTemplate(url.href);
+  const imgPath = join(__dirname, "test.jpg");
+  const imgUrl = pathToFileURL(imgPath);
+  const ab = await template.generate({
+    data1: "fuga",
+    data2: "hoge",
+    data3: "foo",
+    data4: imgUrl.href,
+  });
+  const buffer = Buffer.from(ab);
+  const outpath = "tmp/test3_out.xlsx";
+  writeFileSync(outpath, buffer);
 });
