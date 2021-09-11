e61d51c5c5ed8dcafee7904964a0508b467abb66
Yoshihiro Seki <nikochan2k@gmail.com>
2021-09-11 23:18:27 +0900

    remove unnecessary export

--- src/index-browser.ts
+++ src/index-browser.ts
@@ -4,5 +4,4 @@ ExcelTemplate.readFile = (_path: string): Promise<Buffer> => {
   throw new Error("file protocol is not supported");
 };
 
-export * from "univ-conv";
 export * from "./ExcelTemplate";
